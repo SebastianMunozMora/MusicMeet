013f9b386144c27277756493042b58e963571ab7
seb <smm9308@gmail.com>
2019-10-11 17:01:56 -0500

    massive styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+  #has_many_attached :avatars
+  #def thumbnail input
+   # return self.avatar[input].variant(resize: '100x100!').processed
+  #end
   has_many :signups
   has_many :practices, through: :signups
 end
